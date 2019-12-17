74a336050417a0f12afb78f6892fa5db26922bce
Eero Savukoski <eero.savukoski@edu.tampere.fi>
2019-12-17 14:23:35 +0200

    17.12.2019

--- src/components/Gigs.js
+++ src/components/Gigs.js
@@ -4,7 +4,15 @@ import gigService from '../services/gigs';
 const Gig = ({gig}) => {
     return (
         <div>
-            <li> {gig.artist}</li>
+            <h2> {gig.artist} </h2>
+            <img src={gig.eventAd} alt="band"/>
+            <p>Event: {gig.event}</p>
+            <p>Description: {gig.eventDescription}</p>
+            <p>Date: {gig.date}</p>
+            <p>Address: {gig.address}</p>
+            <p>City: {gig.city}</p>
+            <p>Music style: {gig.musicStyle}</p>
+            <a href={gig.ticketsUrl}>Tickets</a>
         </div>
         )
 }
